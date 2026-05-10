5663166521eecebe53786d5704683978e33812df
Yousef Emad Zuainat <117475632+yousefzuainat@users.noreply.github.com>
2026-05-10 12:15:14 +0300

    Add Data Annotations and relationships to models

--- ecommerce system/Models/Category.cs
+++ ecommerce system/Models/Category.cs
@@ -1,12 +1,17 @@
-﻿namespace ecommerce_system.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace ecommerce_system.Models
 {
     public class Category
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Category Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
         public string Name { get; set; }
 
-        public string Img { get; set; }
+        public string? Img { get; set; }
 
+        public ICollection<Proudect>? Proudects { get; set; }
     }
 }
