d203dc465ffc4a8a73034e090d41d24c8b2d774b
Yousef Emad Zuainat <117475632+yousefzuainat@users.noreply.github.com>
2026-05-10 11:14:13 +0300

    Add project files.

---
+++ ecommerce system/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_system.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
